d6171d98be27c16e1180ef70a4f310bcd2e45a67
emilio barrera <barrera_emilio@hotmail.com>
2016-05-11 07:13:26 -0500

    class repository = ok, inset,update,delete, list = ok

--- src/Classes/UserDAO.java
+++ src/Classes/UserDAO.java
@@ -4,6 +4,7 @@ package Classes;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 
 public class UserDAO 
@@ -44,7 +45,128 @@ public class UserDAO
         catch (SQLException ex) 
         {
           return null;
-        }               
+        }   
         
+                
     }
+    
+   public  static  Response Create(User user)
+   {
+       DBConnection con = new DBConnection();
+       
+       //INSERT INTO user(Email, FirstName, LastName, Password, RolId) VALUES (?,?,?,?,?)
+       
+       try 
+       {
+           String sql = "INSERT INTO user(Email, FirstName, LastName, Password, RolId) VALUES (?,?,?,?,?)";
+           
+           PreparedStatement query = con.getConnection().prepareStatement(sql);
+           query.setString(1, user.getEmail());
+           query.setString(2, user.getFirstName());
+           query.setString(3, user.getLastName());
+           query.setString(4, user.getPassword());
+           query.setInt(5, user.getRolId());
+           
+           query.executeLargeUpdate();
+           
+           con.CloseConecction();
+           
+           return  new Response (true);
+           
+       } 
+       catch (Exception e) 
+       {
+           con.CloseConecction();
+           return  new Response(e.getMessage());
+       }      
+      
+   }
+   
+   public  static  Response Update(User user)
+   {
+       DBConnection con = new DBConnection();
+       
+       try 
+       {
+           String sql = "UPDATE user SET FirstName=?,LastName=?,Password=?,RolId=? WHERE Email=?";
+           
+           PreparedStatement query = con.getConnection().prepareStatement(sql);
+           query.setString(1, user.getFirstName());
+           query.setString(2, user.getLastName());
+           query.setString(3, user.getPassword());
+           query.setInt(4, user.getRolId());
+           query.setString(5, user.getEmail());
+           
+           query.executeUpdate();
+           
+           con.CloseConecction();
+           
+           return new Response(true);
+                   
+       } 
+       catch (Exception e) 
+       {
+           con.CloseConecction();
+           return new Response(e.getMessage());
+       }
+   }
+   
+   public  static  Response Delete(String email)
+   {
+       DBConnection con = new DBConnection();
+       
+       try 
+       {
+           String sql = "DELETE FROM user WHERE Email = ?";
+           PreparedStatement query = con.getConnection().prepareStatement(sql);
+           query.setString(1, email);
+           query.executeUpdate();
+           
+           con.CloseConecction();
+           
+           return  new Response(true);
+       }
+       catch (Exception e)
+       {
+           con.CloseConecction();
+           return new Response(e.getMessage());
+       }
+   }
+   
+   public  static  ArrayList<User> get()
+   {
+       DBConnection con = new DBConnection();
+       
+       try 
+       {
+           ArrayList<User> users = new ArrayList<>();
+           String sql = "Select * form User";
+           PreparedStatement query = con.getConnection().prepareStatement(sql);
+           
+           ResultSet result = query.executeQuery();
+           
+           while(result.next())
+           {
+               User user = new User
+                (
+                        result.getString("Email"),
+                        result.getString("FirstName"),
+                        result.getString("LastName"),
+                        result.getString("Password"),
+                        result.getInt("RolId")
+                );
+               
+               users.add(user);
+           }
+           
+           con.CloseConecction();
+           return users;
+           
+       } 
+       catch (Exception e)
+       {
+           con.CloseConecction();
+           return null;
+       }
+   }
 }
